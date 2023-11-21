42e7af4f9902d3a6ae501524086e9eba3230d98e
WillUK99 <76099444+WillUK99@users.noreply.github.com>
2023-11-21 06:14:23 +0000

    feat: create emails table

---
+++ db/db.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	"github.com/mattn/go-sqlite3"
+)
+
+type EmailEntry struct {
+	Id          int64
+	Email       string
+	ConfirmedAt *time.Time
+	OptOut      bool
+}
+
+func Create(db *sql.DB) {
+	_, err := db.Exec(`
+		CREATE TABLE emails (
+			id 						INTEGER PRIMARY KEY,
+			email 				TEXT UNIQUE,
+			confirmed_at	INTEGER,
+			opt_out 			BOOLEAN NOT NULL DEFAULT FALSE
+		);
+	`)
+	if err != nil {
+		if sqlError, ok := err.(sqlite3.Error); ok {
+			if sqlError.Code != 1 { // 1 = SQLITE_ERROR = table already exists
+				log.Fatal(sqlError)
+			}
+		} else {
+			log.Fatal(err)
+		}
+	}
+}
+
+func emailEntry(row *sql.Rows) (*EmailEntry, error) {
+	var id int64
+	var email string
+	var confirmedAt int64
+	var optOut bool
+
+	err := row.Scan(&id, &email, &confirmedAt, &optOut)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	t := time.Unix(confirmedAt, 0)
+
+	return &EmailEntry{
+		Id:          id,
+		Email:       email,
+		ConfirmedAt: &t,
+		OptOut:      optOut,
+	}, nil
+}
