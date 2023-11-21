058370f737d138f94968d970c0a1e544cbb538b5
WillUK99 <76099444+WillUK99@users.noreply.github.com>
2023-11-21 06:40:32 +0000

    feat: add some crud operations

--- db/db.go
+++ db/db.go
@@ -57,3 +57,102 @@ func emailEntry(row *sql.Rows) (*EmailEntry, error) {
 		OptOut:      optOut,
 	}, nil
 }
+
+func CreateEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(`
+		INSERT INTO emails (email, confirmed_at, opt_out) 
+		VALUES (?, 0, false`, email)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func GetEmail(db *sql.DB, email string) (*EmailEntry, error) {
+	rows, err := db.Query(`
+		SELECT id, email, confirmed_at, opt_out 
+		FROM emails 
+		WHERE email = ?`, email)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	defer rows.Close() // need to close the database connection when the function returns :)
+
+	if rows.Next() {
+		return emailEntry(rows)
+	}
+
+	return nil, nil
+}
+
+func UpdateEmail(db *sql.DB, entry EmailEntry) error {
+	t := time.Now().Unix()
+
+	_, err := db.Exec(`
+		INSERT INTO emails (email, confirmed_at, opt_out)
+		VALUES (?, ?, false)
+		ON CONFLICT(email) DO UPDATE SET confirmed_at = ? opt_out = ? `, entry.Email, t, entry.OptOut, t, entry.OptOut)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+// This app is a mailing list so doesn't actually delete emails, it just sets the opt_out flag to true
+func DeleteEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(`
+		UPDATE emails
+		SET opt_out = true
+		WHERE email = ?`, email)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+type GetEmailBatchQueryParams struct {
+	Offset int
+	Limit  int
+}
+
+func GetEmailBatch(db *sql.DB, params GetEmailBatchQueryParams) ([]EmailEntry, error) {
+	rows, err := db.Query(`
+		SELECT id, email, confirmed_at, opt_out
+		FROM emails
+		ORDER BY id ASC
+		LIMIT ? OFFSET ?`, params.Limit, (params.Offset-1)*params.Limit)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	emails := make([]EmailEntry, 0, params.Limit)
+
+	for rows.Next() {
+		entry, err := emailEntry(rows)
+
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+
+		emails = append(emails, *entry)
+	}
+
+	return emails, nil
+}
